9826d7bd383570e6b5066ea52a773ffc7daa302b
Miswanting <miswanting@users.noreply.github.com>
2019-05-29 22:10:14 +0800

    优化Nav

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,7 +1,7 @@
 <template lang="pug">
-    nav
+    nav(class='nav')
         //- span(class='menu') ≡
-        menu-component
+        span(class='menu') ≡
         title-component(v-if='mode==="single"')
         tag-component(v-else)
         span(class='min') ●
@@ -11,14 +11,12 @@
 
 <script lang="ts">
 import Vue from "vue";
-import MenuComponent from "./Menu.vue";
 import TitleComponent from "./Title.vue";
 import TagComponent from "./Tag.vue";
 
 export default Vue.extend({
   props: ["mode"],
   components: {
-    MenuComponent,
     TitleComponent,
     TagComponent
   }
