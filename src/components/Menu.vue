a704ead923a510353548d67c1cdc368026fc1d07
Miswanting <453542772@qq.com>
2019-05-29 09:41:46 +0800

    Create Menu.vue

---
+++ src/components/Menu.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+    span(class='menu' v-on:click='click') ≡
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["name", "initialEnthusiasm"],
+  data() {
+    return {
+      
+    };
+  },
+  methods: {
+    click: function() {
+      console.log("123");
+    }
+  },
+  computed: {
+    exclamationMarks(): string {
+      return Array(this.enthusiasm + 1).join("!");
+    }
+  }
+});
+</script>
