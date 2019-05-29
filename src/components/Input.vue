a44337f627bac5e37af03054270dffa82b5f4c47
Miswanting <miswanting@users.noreply.github.com>
2019-05-29 22:09:32 +0800

    新增Input控件

---
+++ src/components/Input.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+    div(class="input")
+        span >
+        span(class="input-value" contenteditable="true" @keydown.enter.prevent="submit" @input="change")
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  //   props: ["value"],
+  data() {
+    return {
+      cmd: ""
+    };
+  },
+  methods: {
+    change: function(e: any) {
+      console.log(e.target.innerText);
+      this.cmd = e.target.innerText;
+    },
+    submit: function(e: any) {
+      console.log(e.target.innerText);
+      this.$emit("input", e.target.innerText);
+      e.target.innerText = "";
+    }
+  }
+});
+</script>
