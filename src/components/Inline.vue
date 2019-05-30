27e7974b2de9f1f5baf1d17040a0e97ec627aa0b
Miswanting <453542772@qq.com>
2019-05-30 16:39:52 +0800

    Create Inline.vue

---
+++ src/components/Inline.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+    text-component(v-if="data.type=='text'" :data="data")
+    link-component(v-else-if="data.type=='link'" :data="data")
+    button-component(v-else-if="data.type=='button'" :data="data")
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import TextComponent from "./Text.vue";
+import LinkComponent from "./Link.vue";
+import ButtonComponent from "./Button.vue";
+
+export default Vue.extend({
+  props: ["data"],
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {
+    TextComponent,
+    LinkComponent,
+    ButtonComponent
+  }
+});
+</script>
