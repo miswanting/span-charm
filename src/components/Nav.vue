7ee5b91b521e74ebbf3c4b6b0c7d0a28d1dfb9ab
Miswanting <453542772@qq.com>
2019-05-30 16:42:41 +0800

    优化标题

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,9 +1,8 @@
 <template lang="pug">
     nav(class='nav')
-        //- span(class='menu') ≡
         span(class='menu') ≡
-        title-component(v-if='mode==="single"')
-        tag-component(v-else)
+        span(v-if='data==="single"')
+        span(v-else)
         span(class='min') ●
         span(class='max') ●
         span(class='close') ●
@@ -11,11 +10,12 @@
 
 <script lang="ts">
 import Vue from "vue";
+
 import TitleComponent from "./Title.vue";
 import TagComponent from "./Tag.vue";
 
 export default Vue.extend({
-  props: ["mode"],
+  props: ["data"],
   components: {
     TitleComponent,
     TagComponent
