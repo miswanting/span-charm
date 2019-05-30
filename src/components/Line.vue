2dd699d05b9a6a53b60421480942b342b1eed382
Miswanting <453542772@qq.com>
2019-05-30 16:39:57 +0800

    Create Line.vue

---
+++ src/components/Line.vue
@@ -0,0 +1,21 @@
+<template lang="pug">
+    div(class='line')
+        inline-component(v-for="child in data.child" :key="child.id" :data="child")
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import InlineComponent from "./Inline.vue";
+
+export default Vue.extend({
+  props: ["data"],
+  data() {
+    return {};
+  },
+  methods: {},
+  components: {
+    InlineComponent
+  }
+});
+</script>
