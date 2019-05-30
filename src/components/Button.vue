9d6ec6025192c320bed1449e46ce3912e9701042
Miswanting <453542772@qq.com>
2019-05-30 15:31:19 +0800

    Create Button.vue

---
+++ src/components/Button.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+    span(class='button') {{data.value}}
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["data"]
+});
+</script>
