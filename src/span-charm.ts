de81363c0a35429748a944fdae7a52977cc5ff1e
Miswanting <453542772@qq.com>
2019-05-30 16:41:25 +0800

    编造数据结构

--- src/span-charm.ts
+++ src/span-charm.ts
@@ -9,9 +9,47 @@ let v = new Vue({
     </div>
     `,
     data: function () {
-        return {
-            mode: "single",
-            child: []
+        return { // system
+            type: 'platform',
+            systemData: {
+                mode: "single",
+            },
+            programs: [
+                { // program
+                    type: 'program',
+                    name: "erajs",
+                    title: "TITLE",
+                    nav: {},
+                    container: {
+                        pages: [
+                            { // page
+                                type: 'page',
+                                lines: [
+                                    { // line
+                                        type: 'line',
+                                        child: [
+                                            { // text
+                                                type: 'text',
+                                                value: 'test text'
+                                            },
+                                            { // link
+                                                type: 'link',
+                                                value: 'test link'
+                                            },
+                                            { // button
+                                                type: 'button',
+                                                value: 'test button'
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                }
+            ],
+            lastPage: null,
+            lastLine: null,
         }
     },
     components: {
