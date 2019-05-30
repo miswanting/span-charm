74a9998100fa5bb4ef2110255791232dec9e5cb8
Miswanting <453542772@qq.com>
2019-05-30 16:40:05 +0800

    Create Text.vue

---
+++ src/components/Text.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+    span(class='text') {{data.value}}
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["data"]
+});
+</script>
