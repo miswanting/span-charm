0e9cc8f874970fff92f88df25ddf564c9373d8fe
Miswanting <453542772@qq.com>
2019-05-30 16:40:03 +0800

    Create Page.vue

---
+++ src/components/Page.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+    div(class="page")
+        line-component(v-for="line in data.lines" :key="line.id" :data="line") 
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+import LineComponent from "./Line.vue";
+
+export default Vue.extend({
+  props: ["data"],
+  components: {
+    LineComponent
+  }
+});
+</script>
