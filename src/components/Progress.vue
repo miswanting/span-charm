29c18c4c6a18263f38fb40308b3aae696d18be97
Miswanting <453542772@qq.com>
2019-05-31 15:40:59 +0800

    Create Progress.vue

---
+++ src/components/Progress.vue
@@ -0,0 +1,12 @@
+<template lang="pug">
+    span(class="progress" now=33 max=100 width="100px")
+        span(class="bar")
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["initData"]
+});
+</script>
