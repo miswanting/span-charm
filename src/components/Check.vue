aecc19a81fac2bc4f0ca13bd4c00fc5066780eaf
Miswanting <453542772@qq.com>
2019-05-31 15:40:56 +0800

    Create Check.vue

---
+++ src/components/Check.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+    span(class='check' :class="{active: $data.value}" @click="click") {{initData.text}}
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: ["initData"],
+  data() {
+    return {
+      data: this.$props.initData
+    };
+  },
+  methods: {
+    click: function(e) {
+      this.$data.value = !this.$data.value;
+      console.log(this.$data.value);
+    }
+  }
+});
+</script>
