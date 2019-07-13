4dae25f491d735f32694d6ae79a0a0e89a790acc
Miswanting <miswanting@users.noreply.github.com>
2019-07-13 20:13:46 +0800

    go

---
+++ src/span-charm-vue.ts
@@ -0,0 +1,187 @@
+import Vue from "vue";
+import WindowComponent from "./vue/Window.vue";
+import './span-charm.sass'
+let v = new Vue({
+    el: "#app",
+    template: `
+    <window-component :data=$data />
+    `,
+    data: function () {
+        return { // system
+            type: 'platform',
+            systemData: {
+                mode: "single",
+            },
+            programs: [
+                { // program
+                    type: 'program',
+                    name: "erajs",
+                    title: "TITLE",
+                    nav: {},
+                    container: {
+                        pages: [
+                            { // page
+                                type: 'page',
+                                lines: [
+                                    { // line
+                                        type: 'line',
+                                        child: [
+                                            { // text
+                                                type: 'text',
+                                                value: '    Span Charm [Ver 0.1.0-190531]'
+                                            }
+                                        ]
+                                    },
+                                    { // line
+                                        type: 'line',
+                                        child: [
+                                            { // text
+                                                type: 'text',
+                                                value: '    Text Component: '
+                                            },
+                                            { // text
+                                                type: 'text',
+                                                value: 'TEXT'
+                                            },
+                                        ]
+                                    },
+                                    { // line
+                                        type: 'line',
+                                        child: [
+                                            { // text
+                                                type: 'text',
+                                                value: '    Link Component: '
+                                            },
+                                            { // link
+                                                type: 'link',
+                                                value: 'LINK'
+                                            },
+                                        ]
+                                    },
+                                    { // line
+                                        type: 'line',
+                                        child: [
+                                            { // text
+                                                type: 'text',
+                                                value: '    Button Component: '
+                                            },
+                                            { // button
+                                                type: 'button',
+                                                value: 'BUTTON'
+                                            },
+                                        ]
+                                    },
+                                    { // line
+                                        type: 'line',
+                                        child: [
+                                            { // text
+                                                type: 'text',
+                                                value: '    Progress Component: '
+                                            },
+                                            { // progress
+                                                type: 'progress',
+                                                now: 33,
+                                                max: 100,
+                                                width: 100
+                                            },
+                                        ]
+                                    },
+                                    { // line
+                                        type: 'line',
+                                        child: [
+                                            { // text
+                                                type: 'text',
+                                                value: '    Rate Component: '
+                                            },
+                                            { // rate
+                                                type: 'rate',
+                                                now: 3,
+                                                max: 5
+                                            },
+                                        ]
+                                    },
+                                    { // line
+                                        type: 'line',
+                                        child: [
+                                            { // text
+                                                type: 'text',
+                                                value: '    Checkbox Component: '
+                                            },
+                                            { // check
+                                                type: 'check',
+                                                text: 'SelectA',
+                                                value: false
+                                            },
+                                            { // check
+                                                type: 'check',
+                                                text: 'SelectB',
+                                                value: true
+                                            },
+                                            { // check
+                                                type: 'check',
+                                                text: 'SelectC',
+                                                value: false
+                                            },
+                                        ]
+                                    },
+                                    { // line
+                                        type: 'line',
+                                        child: [
+                                            { // text
+                                                type: 'text',
+                                                value: '    Radio Component: '
+                                            },
+                                            { // radio
+                                                type: 'radio',
+                                                index: 0,
+                                                text: ['SelectA', 'SelectB', 'SelectC']
+                                            }
+                                        ]
+                                    }
+                                ]
+                            }
+                        ]
+                    },
+                }
+            ],
+            lastPage: null,
+            lastLine: null,
+        }
+    },
+    components: {
+        WindowComponent
+    }
+});
+function newProgram(name: string = 'N/A') {
+    let newProgram = {
+        type: 'program',
+        name: name,
+        hash: '',
+        title: name,
+        nav: {},
+        container: {
+            pages: []
+        }
+    }
+    v.$data.programs.push(newProgram)
+    return newProgram.hash
+}
+function newPage(programHash: string) {
+    let program = getHashProgram(programHash)
+}
+function newLine(programHash: string) {
+    let program = getHashProgram(programHash)
+}
+function newComponent(programHash: string) {
+    let program = getHashProgram(programHash)
+}
+function getHashProgram(programHash: string) {
+    for (let i = 0; i < v.$data.programs.length; i++) {
+        const program = v.$data.programs[i];
+        if (program.hash == programHash) {
+            return program
+        }
+    }
+}
+let hash = newProgram('test')
+newPage(hash)
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,118 @@
 const path = require('path')
 const VueLoaderPlugin = require('vue-loader/lib/plugin')
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-
-
+vue = {
+    entry: './src/span-charm-vue.ts',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'span-charm-vue.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/,
+                options: {
+                    appendTsSuffixTo: [/\.vue$/],
+                }
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        // Since sass-loader (weirdly) has SCSS as its default parse mode, we map
+                        // the "scss" and "sass" values for the lang attribute to the right configs here.
+                        // other preprocessors should work out of the box, no loader config like this necessary.
+                        'scss': 'vue-style-loader!css-loader!sass-loader',
+                        'sass': 'vue-style-loader!css-loader!sass-loader?indentedSyntax',
+                    }
+                    // other vue-loader options go here
+                }
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }, {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            hmr: process.env.NODE_ENV === 'development',
+                        },
+                    },
+                    // "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ]
+            },
+            {
+                test: /\.pug$/,
+                loader: 'pug-plain-loader'
+            },
+            {
+                test: /\.(png|jpg|gif|ttf)$/,
+                use: [
+                    {
+                        loader: 'file-loader'
+                    },
+                ],
+            },
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.vue', '.json'],
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    plugins: [
+        // 请确保引入这个插件！
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "span-charm.css",
+            chunkFilename: "span-charm.css"
+        })
+    ]
+}
+react = {
+    entry: './src/span-charm-react.tsx',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'span-charm-react.js'
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: 'awesome-typescript-loader',
+            },
+            {
+                enforce: "pre",
+                test: /\.js$/,
+                loader: "source-map-loader"
+            }
+        ]
+    },
+    resolve: {
+        // Add '.ts' and '.tsx' as resolvable extensions.
+        extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    }
+}
 script = {
     entry: './src/span-charm.ts',
     mode: 'development',
@@ -162,4 +272,4 @@ style = {
     ]
 }
 // module.exports = [script, style]
-module.exports = script
+module.exports = [vue, react]
