41223012946d58de0f2418b2783c184a2345b57d
(ง •_•)ง <miswanting@users.noreply.github.com>
2019-12-18 22:21:58 +0800

    Rewrite

--- !Debug(Watch).bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Debug(Watch)
-cls
-call sass --watch span-charm.sass span-charm.css
-ping -t 5 localhost > nul 
--- !Debug.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-title Debug
-cls
-call sass span-charm.sass span-charm.css
-ping -t 5 localhost > nul 
